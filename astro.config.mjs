2e2c42e716f0d3a493c41fd325649400931b605a
Pavel Sušický <me@frontendista.cz>
2024-08-25 15:00:27 +0200

    chore(deps): add Svelte 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
 	experimental: {
 		contentLayer: true,
 		contentIntellisense: true,
 	},
-	integrations: [mdx()],
+	integrations: [mdx(), svelte()],
 });
