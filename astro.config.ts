d079061696a67068d3f84586163dc97381779e10
Pavel Sušický <me@frontendista.cz>
2024-08-26 17:01:18 +0200

    chore(config): add optimizations

---
+++ astro.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+import blist from "browserslist-to-esbuild"
+
+import { browserslist } from "./package.json";
+
+// https://astro.build/config
+export default defineConfig({
+	experimental: {
+		contentLayer: true,
+		contentIntellisense: true,
+	},
+	vite: {
+		build: {
+			assetsInlineLimit: 0,
+			cssMinify: "lightningcss",
+			target: blist(browserslist),
+			rollupOptions: {
+				plugins: [],
+				output: {
+					entryFileNames: "assets/[hash:16].js",
+					assetFileNames: "assets/[hash:16][extname]",
+				},
+			},
+		},
+	},
+	server: {
+		host: true,
+		open: true,
+	},
+	integrations: [mdx(), svelte(), tailwind()],
+});
