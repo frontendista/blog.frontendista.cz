8d135933eb78c9108655f01fddb93e5bfb689c92
Pavel Sušický <me@frontendista.cz>
2025-07-30 00:05:15 +0200

    feat(content): add dates to writing

--- data/writings/test.md
+++ data/writings/test.md
@@ -1,6 +1,7 @@
 ---
 title: How to create a new application
 description: This ia a tutorial how to create new application in the modern world
+publishedAt: 2025-07-29T21:54:25+0000
 series: prasokod
 authors:
 - susickypavel
--- src/collections/writings.ts
+++ src/collections/writings.ts
@@ -8,19 +8,32 @@ const WRITING_MAX_TITLE_LENGTH = 60;
 const WRITING_MIN_DESCRIPTION_LENGTH = 60;
 const WRITING_MAX_DESCRIPTION_LENGTH = 120;
 
+const schema = z.object({
+	authors: z.array(reference("authors"))
+		.min(1, "At least one author is required")
+		// NOTE: https://github.com/withastro/astro/issues/13268
+		.refine(authors => authors.some(author => authors_db.map(author => author.id).includes(author.id)), "Author must be defined in authors.json"),
+	publishedAt: z.coerce.date(),
+	updatedAt: z.coerce.date().optional(),
+	description: z.string().trim()
+		.min(WRITING_MIN_DESCRIPTION_LENGTH, `Description must be at least ${WRITING_MIN_DESCRIPTION_LENGTH} characters long`)
+		.max(WRITING_MAX_DESCRIPTION_LENGTH, `Description must be at most ${WRITING_MAX_DESCRIPTION_LENGTH} characters long`),
+	series: reference("series").optional(),
+	title: z.string().trim()
+		.min(WRITING_MIN_TITLE_LENGTH, `Title must be at least ${WRITING_MIN_TITLE_LENGTH} characters long`)
+		.max(WRITING_MAX_TITLE_LENGTH, `Title must be at most ${WRITING_MAX_TITLE_LENGTH} characters long`)
+});
+
+type Schema = z.infer<typeof schema>;
+
+function isUpdatedAtAfterPublishedAt(data: Schema) {
+	return !data.updatedAt || data.updatedAt >= data.publishedAt;
+};
+
 export const writings = defineCollection({
 	loader: glob({ base: "./data/writings", pattern: "**/*.md" }),
-	schema: z.object({
-		authors: z.array(reference("authors"))
-			.min(1, "At least one author is required")
-			// NOTE: https://github.com/withastro/astro/issues/13268
-			.refine(authors => authors.some(author => authors_db.map(author => author.id).includes(author.id)), "Author must be defined in authors.json"),
-		description: z.string().trim()
-			.min(WRITING_MIN_DESCRIPTION_LENGTH, `Description must be at least ${WRITING_MIN_DESCRIPTION_LENGTH} characters long`)
-			.max(WRITING_MAX_DESCRIPTION_LENGTH, `Description must be at most ${WRITING_MAX_DESCRIPTION_LENGTH} characters long`),
-		series: reference("series").optional(),
-		title: z.string().trim()
-			.min(WRITING_MIN_TITLE_LENGTH, `Title must be at least ${WRITING_MIN_TITLE_LENGTH} characters long`)
-			.max(WRITING_MAX_TITLE_LENGTH, `Title must be at most ${WRITING_MAX_TITLE_LENGTH} characters long`)
+	schema: schema.refine(isUpdatedAtAfterPublishedAt, {
+		message: "Updated date must be after publishedAt date",
+		path: ["updatedAt"]
 	})
 });
