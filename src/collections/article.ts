9cc1b125dd0173ceaad1a1672364d7a608af2b9a
Pavel Sušický <me@frontendista.cz>
2025-07-31 15:09:15 +0200

    feat(article): add category and tags

--- src/collections/article.ts
+++ src/collections/article.ts
@@ -13,23 +13,43 @@ const ARTICLE_MAX_DESCRIPTION_LENGTH = 120;
 const ARTICLE_CANONICAL_URL_MIN_LENGTH = ARTICLE_MIN_TITLE_LENGTH;
 const ARTICLE_CANONICAL_URL_MAX_LENGTH = ARTICLE_MAX_TITLE_LENGTH * 2;
 
+const MIN_TAG_STR_LENGTH = 2;
+const MAX_TAG_STR_LENGTH = 20;
+
+const MIN_TAGS_LENGTH = 2;
+const MAX_TAGS_LENGTH = 5;
+
+const tagSchema = z.string().trim()
+	.min(MIN_TAG_STR_LENGTH, `Tags must be at least ${MIN_TAG_STR_LENGTH} characters long`)
+	.max(MAX_TAG_STR_LENGTH, `Tags must be at most ${MAX_TAG_STR_LENGTH} characters long`);
+
 const baseSchema = z.object({
 	authors: z.array(reference("authors"))
 		.min(1, "At least one author is required")
 		// NOTE: https://github.com/withastro/astro/issues/13268
 		.refine(authors => authors.some(author => authors_db.map(author => author.id).includes(author.id)), "Author must be defined in authors.json"),
+
 	canonical_url: z.string().trim()
 		.min(ARTICLE_CANONICAL_URL_MIN_LENGTH, `Canonical URL must be at least ${ARTICLE_CANONICAL_URL_MIN_LENGTH} characters long`)
 		.max(ARTICLE_CANONICAL_URL_MAX_LENGTH, `Canonical URL must be at most ${ARTICLE_CANONICAL_URL_MAX_LENGTH} characters long`)
 		.optional(),
+
+	category: z.enum(["tech", "health"]),
+	tags: z.array(tagSchema)
+		.min(MIN_TAGS_LENGTH, `At least ${MIN_TAGS_LENGTH} tags are required`)
+		.max(MAX_TAGS_LENGTH, `At most ${MAX_TAGS_LENGTH} tags are allowed`),
+
 	published_at: z.coerce.date(),
 	updated_at: z.coerce.date().optional(),
+
 	description: z.string().trim()
 		.min(ARTICLE_MIN_DESCRIPTION_LENGTH, `Description must be at least ${ARTICLE_MIN_DESCRIPTION_LENGTH} characters long`)
 		.max(ARTICLE_MAX_DESCRIPTION_LENGTH, `Description must be at most ${ARTICLE_MAX_DESCRIPTION_LENGTH} characters long`),
+
 	draft: z.boolean(),
 	language: z.enum(locales),
 	series: reference("series").optional(),
+
 	title: z.string().trim()
 		.min(ARTICLE_MIN_TITLE_LENGTH, `Title must be at least ${ARTICLE_MIN_TITLE_LENGTH} characters long`)
 		.max(ARTICLE_MAX_TITLE_LENGTH, `Title must be at most ${ARTICLE_MAX_TITLE_LENGTH} characters long`)
