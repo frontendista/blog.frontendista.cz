6e2cbba723fcb655cb76b587bdb89f066885607d
Pavel Sušický <me@frontendista.cz>
2025-08-07 16:47:43 +0200

    refactor

---
+++ src/pages/api/favicon.ts
@@ -0,0 +1,37 @@
+import type { APIRoute } from "astro";
+import { ResultAsync } from "neverthrow";
+import { object, parseAsync, pipe, string, trim, url } from "valibot";
+
+const schema = object({
+	url: pipe(
+		string(),
+		trim(),
+		url()
+	)
+});
+
+// async function fetchFavicon(url: string) {
+// 	try {
+// 		const response = await fetch(`https://www.google.com/s2/favicons?domain=example.com&sz=64`, {});
+
+// 		if (!response.ok) {
+// 			return err(new Error("Failed to fetch favicon"));
+// 		}
+
+// 		ok("test");
+// 	}
+// 	catch {
+// 		return err({ message: "An error occurred while fetching the favicon" });
+// 	}
+// }
+
+export const GET: APIRoute = async ({ params }) => {
+	const result = await ResultAsync.fromPromise(parseAsync(schema, { url }), e => e);
+
+	if (result.isErr()) {
+		return new Response(JSON.stringify({ error: "Invalid URL" }), {
+			status: 400,
+			headers: { "Content-Type": "application/json" }
+		});
+	}
+};
